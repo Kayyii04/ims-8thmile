fc88d086f33ef5eb490ade54ab08a6a744c897bd
Kayyii04 <kylenagares2356@gmail.com>
2026-03-03 13:24:32 +0800

    Update database configuration for InfinityFree

--- config.php
+++ config.php
@@ -1,8 +1,9 @@
 <?php
-$host = "localhost";
-$db_user = "root";
-$db_pass = "";
-$db_name = "8th_mile_db";
+// Database configuration for InfinityFree
+$host = "sql308.infinityfree.com"; // Get this from your 'MySQL Hostname'
+$db_user = "if0_41224072";        // Get this from your 'MySQL Username'
+$db_pass = "8thmileims"; // The password you use to log in to InfinityFree
+$db_name = "if0_41224072_8th_mile_db"; // The full database name you created in the panel
 
 $conn = mysqli_connect($host, $db_user, $db_pass, $db_name);
 
